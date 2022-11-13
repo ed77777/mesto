fbd9efb8affd53eb38c5cddb629e11cb08bc23b2
eduard.nogovitsyn <ed77777@yandex.ru>
2022-11-13 14:43:53 +0300

    13112022-1

--- scripts/index.js
+++ scripts/index.js
@@ -4,38 +4,27 @@ const profileTitleElem = document.querySelector(".profile__title");
 const profileDescrElem = document.querySelector(".profile__description");
 const inputUserNameElem = document.querySelector(".popup__input_input_1");
 const inputDescrElem = document.querySelector(".popup__input_input_2");
-
 const popupFormElem = document.querySelector(".popup__form");
+const buttonCloseIcon = document.querySelector(".popup__close-icon");
 
-popupFormElem.addEventListener("submit", submitpopupForm);
-
-function  submitpopupForm(evt) {
+function submitpopupForm(evt) {
   evt.preventDefault();
-  popupElem.classList.remove("popup_opened");
+  closePopup();
+  // popupElem.classList.remove("popup_opened");
   profileTitleElem.textContent = inputUserNameElem.value;
   profileDescrElem.textContent = inputDescrElem.value;
 }
 
-function  openPopup() {
+function openPopup() {
   popupElem.classList.add("popup_opened");
   inputUserNameElem.value = profileTitleElem.innerText;
   inputDescrElem.value = profileDescrElem.innerText;
 }
 
-buttonEdit.addEventListener("click", openPopup);
-
-// function  closeAndSavePopup() {
-//   popupElem.classList.remove("popup_opened");
-//   profileTitleElem.textContent = popupInput1Elem.value;
-//   profileDescrElem.textContent = popupInput2Elem.value;
-// }
-
-// const buttonSave = document.querySelector(".popup__button-save");
-// buttonSave.addEventListener("click", closeAndSavePopup);
-
-function  closePopup() {
+function closePopup() {
   popupElem.classList.remove("popup_opened");
 }
 
-const buttonCloseIcon = document.querySelector(".popup__closeIcon");
 buttonCloseIcon.addEventListener("click", closePopup);
+buttonEdit.addEventListener("click", openPopup);
+popupFormElem.addEventListener("submit", submitpopupForm);
